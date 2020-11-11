1dfca1a27d9f93b046d3b19df8cb783b183b2ac3
ziyodibr <ziyodji1999@gmail.com>
2020-11-11 16:26:01 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import './App.css';
 
 import Footer from './Components/Footer/footer';
 import Body from './Components/Body/body';
+import CarouselPage from './Components/Carousel/carousel';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header"> */}
-       <Navbar/>
-    
-      <Body/>
+      {/* <Navbar/> */}
+      <CarouselPage/>
+      {/* <Body/>
 
    
-      <Footer/>
+      <Footer/> */}
   
     </div>
   );
