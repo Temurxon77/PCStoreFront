0649305886abf190136830967246a5c3179c21b3
ziyodibr <ziyodji1999@gmail.com>
2020-12-07 22:04:32 +0500

    sdf

--- src/Components/Body/body.js
+++ src/Components/Body/body.js
@@ -3,10 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AnimatedCard from "@sl-codeblaster/react-3d-animated-card";
 import "@sl-codeblaster/react-3d-animated-card/dist/index.css";
 import './body.css';
-import capture from '../Images/orig.png';
-import photo1 from '../Images/1.png';
-import photo2 from '../Images/nabor.jpg';
-import photo3 from '../Images/naushniki.jpg'
+import capture from '../Images/motherboards.jpg';
+import capture1 from '../Images/graphic.jpg';
+import capture2 from '../Images/processor.jpg';
+import capture3 from '../Images/cases.jpg';
+import capture4 from '../Images/ozu.jpg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,40 +20,34 @@ const useStyles = makeStyles((theme) => ({
     export default function Body() {
     const classes = useStyles();
     const [value, setValue] = React.useState(0);  
-  
-    
+   
       return (
     
         <div class="wrapper">
              
-            <h2><strong>Laptops</strong></h2>
+            <h2><strong>PC Components</strong></h2>
             <div class="cards">
                 <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/1.jpg" />
-                    <figcaption>Dota 2</figcaption>
+                    <img src={capture} />
+                    <figcaption>Motherboards</figcaption>
                 </figure>
                 <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/2.jpg" />
-                    <figcaption>Stick Fight</figcaption>
+                    <img src={capture1} />
+                    <figcaption>Graphic Cards</figcaption>
                 </figure>
                 <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/3.jpg" />
-                    <figcaption>Minion Masters</figcaption>
+                    <img src={capture2} />
+                    <figcaption>Processors</figcaption>
                 </figure>
                 <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/4.jpg" />
-                    <figcaption>KoseBoz!</figcaption>
+                    <img src={capture3} />
+                    <figcaption>PC Cases</figcaption>
                 </figure>
                 <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/1.jpg" />
-                    <figcaption>Dota 2</figcaption>
+                    <img src={capture4} />
+                    <figcaption>RAMS</figcaption>
                 </figure>
-                <figure class="card">
-                    <img src="https://mrreiha.keybase.pub/codepen/hover-fx/2.jpg" />
-                    <figcaption>Stick Fight</figcaption>
-                </figure>
-              
-   
+               
             </div>
             <h2><strong>What's new?</strong></h2>
              <div class="news">
@@ -76,7 +71,6 @@ const useStyles = makeStyles((theme) => ({
                 </figure>
             </div>
         </div>
-
       )
     };
     
