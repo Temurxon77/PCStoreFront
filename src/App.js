b7026e24a8d25aaa18e0482b08d60531845077b7
ziyodibr <ziyodji1999@gmail.com>
2020-12-28 18:09:14 +0300

    фыв

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   // window.onscroll = function() { scrollEvent() };
   return (
     <div className="App">
-    {/* <Preloader/>  */}
+    <Preloader/> 
       <Navbar/>    
       
       <MainCarouselPage/>
