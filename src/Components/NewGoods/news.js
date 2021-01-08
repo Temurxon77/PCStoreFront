a78cae4d7385e0ff3f58e336fbcbae0be7b07bce
ziyodibr <ziyodji1999@gmail.com>
2021-01-08 18:08:16 +0300

    xcvv

--- src/Components/NewGoods/news.js
+++ src/Components/NewGoods/news.js
@@ -3,6 +3,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import capture from '../Images/n1.png';
 import capture1 from '../Images/n2.jpg';
+import capture2 from '../Images/consultation.jpg'
 import '../NewGoods/news.css'
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -24,24 +25,26 @@ import { CenterFocusStrong } from '@material-ui/icons';
     return (
         <div className="wrapperr" style={{backgroundColor: '#0E0F0F'}}>
         
-                <div
-                    data-aos="zoom-in"
-                    data-aos-offset="100"
-                    data-aos-delay="5000"
+                <div 
+                    data-aos="fade-up-right"
+                    data-aos-offset="1"
+                    data-aos-delay="50000"
                     data-aos-duration="5000"
-                    data-aos-easing="ease-in-out"
+                    data-aos-easing="ease-in-out-cubic"
                     data-aos-mirror="true"
                     data-aos-once="false"
                     data-aos-anchor-placement="center-bottom" 
-                    
+                    style={{paddingTop: '70px'}}
                 >   
+                {/* <input type="text" style={{backgroundImage: capture2}}>mfksdjfkj</input> */}
                     
-                    <img src={capture} style={{height: '150px'}}/> 
-                          
+                    <div className="inputdiv" style={{paddingTop: '100px'}}> <input style={{}} /></div>
+                    {/* <img src={capture2} style={{width: '1200px', height: '450px', marginTop: '50px'}}/>  */}
+                        
                 </div>    
         
 
-        <div className="captures" style={{display: 'flex', marginRight: 'auto', marginLeft: 'auto'}}>
+        <div className="captures" style={{display: 'flex', width: 'auto'}}>
 
             <div>
                 <div className="name" style={{paddingRight: '10px', marginTop: '80px', fontSize: '40px'}}>News</div>
@@ -57,7 +60,7 @@ import { CenterFocusStrong } from '@material-ui/icons';
                 data-aos-mirror="true"
                 data-aos-once="false"
                 data-aos-anchor-placement="center-bottom" 
-                style={{paddingRight: '20px', marginTop: '50px'}}
+                style={{paddingRight: '20px', marginTop: '50px', margin: 'auto'}}
             >   
                 <div className="name" style={{padding: '10px', textAlign: 'left'}}>07/01/2020</div>
                 <img src={capture} style={{height: '150px'}}/> 
@@ -73,7 +76,7 @@ import { CenterFocusStrong } from '@material-ui/icons';
                 data-aos-mirror="true"
                 data-aos-once="false"
                 data-aos-anchor-placement="center-bottom"  
-                style={{paddingRight: '20px', marginTop: '50px'}}           
+                style={{paddingRight: '20px', marginTop: '50px', margin: 'auto'}}           
             >  
                 <div className="name" style={{padding: '10px', textAlign: 'left'}}>07/01/2020</div>
                 <img src={capture1} style={{height: '150px'}}/>
@@ -91,7 +94,7 @@ import { CenterFocusStrong } from '@material-ui/icons';
                 data-aos-anchor-placement="center-bottom"  
                 style={{paddingRight: '20px', marginTop: '50px'}}           
             >
-                <div className="name" style={{padding: '10px', textAlign: 'left'}}>07/01/2020</div>
+                <div className="name" style={{padding: '10px', textAlign: 'left', margin: 'auto'}}>07/01/2020</div>
                 <img src={capture1} style={{height: '150px'}}/>
                 <div className="name">Playstation 5</div> 
             </div>               
